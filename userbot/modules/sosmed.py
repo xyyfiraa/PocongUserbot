69f6f293ab2b990952ded874def47ad6e77d4a94
pocong <81405011+poocong@users.noreply.github.com>
2022-06-02 09:46:02 +0700

    [ + ] sosmed: update

--- userbot/modules/sosmed.py
+++ userbot/modules/sosmed.py
@@ -12,7 +12,7 @@ from userbot import CMD_HELP
 from userbot.utils import edit_delete, edit_or_reply, poci_cmd
 
 
-@poci_cmd(pattern="ig(?: |$)(.*)")
+@poci_cmd(pattern="vig(?: |$)(.*)")
 async def _(event):
     xxnx = event.pattern_match.group(1)
     if xxnx:
@@ -51,7 +51,7 @@ async def _(event):
         await xx.delete()
 
 
-@poci_cmd(pattern="tiktok(?: |$)(.*)")
+@poci_cmd(pattern="vtt(?: |$)(.*)")
 async def _(event):
     xxnx = event.pattern_match.group(1)
     if xxnx:
