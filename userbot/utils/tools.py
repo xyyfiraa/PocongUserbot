eb21b240238c770ac173ae29cb040b9745016b4b
pocong <81405011+poocong@users.noreply.github.com>
2022-08-03 18:18:16 +0700

    idk

--- userbot/utils/tools.py
+++ userbot/utils/tools.py
@@ -29,7 +29,6 @@ import time
 from os.path import basename
 from typing import Optional, Tuple, Union
 
-from emoji import get_emoji_regexp
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 from html_telegraph_poster import TelegraphPoster
